18838a3fb7eaf69ac40b134155c59cf364912a58
Simon Baumann <simonbmnn@gmail.com>
2023-08-07 13:29:45 +0200

    inital commit

---
+++ backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "---------- rpool_data ---------" > backup_log.txt
+syncoid --sendoptions="w" --no-privilege-elevation --no-sync-snap --skip-parent -r --sshport 2425 syncoid@pve.home.arpa:rpool/data backup/rpool_data >> backup_log.txt
+echo "---------- nc-disk ---------" >> backup_log.txt
+syncoid --sendoptions="w" --no-privilege-elevation --no-sync-snap --skip-parent -r --sshport 2425 syncoid@pve.home.arpa:nc-disk backup/nc-disk >> backup_log.txt
+echo "---------- shelf_disks ---------" >> backup_log.txt
+syncoid --sendoptions="w" --no-privilege-elevation --no-sync-snap --skip-parent -r --sshport 2425 syncoid@pve.home.arpa:shelf/disks backup/shelf_disks >> backup_log.txt
+echo "---------- rpool_data ---------" >> backup_log.txt
+syncoid --sendoptions="w" --no-privilege-elevation --no-sync-snap --skip-parent -r --sshport 2425 syncoid@pve.home.arpa:shelf/data backup/shelf_data >> backup_log.txt
+#echo "---------- LOG -----------" >> backup_log.txt
+
+#cat /var/log/syslog | grep backup.sh >> backup_log.txt
+
+#echo "---------- LOG -----------" >> backup_log.txt
+sudo sanoid >> backup_log.txt
+
+#send contents of backup_log.txt per telegram
+python3 telegram_bot.py
+
+# RTC WAKE UP
+# Takes a 24hour time HH:MM as its argument
+# Example:
+# suspend_until 9:30
+# suspend_until 18:45
+
+# ------------------------------------------------------
+# Argument check
+if [ $# -lt 1 ]; then
+    echo "Usage: suspend_until HH:MM"
+    exit
+fi
+
+# Check whether specified time today or tomorrow
+DESIRED=$((`date +%s -d "$1"`))
+NOW=$((`date +%s`))
+if [ $DESIRED -lt $NOW ]; then
+    DESIRED=$((`date +%s -d "$1"` + 24*60*60))
+fi
+DESIRED=$(($DESIRED + $2*24*60*60))
+
+# Kill rtcwake if already running
+#sudo killall rtcwake
+
+# Set RTC wakeup time
+# N.B. change "mem" for the suspend option
+# find this by "man rtcwake"
+sudo rtcwake -l -m off -t $DESIRED &
+
+# feedback
+echo "Shutdown..."
+
+# give rtcwake some time to make its stuff
+sleep 2
+
---
+++ telegram_bot.py
@@ -0,0 +1,49 @@
+import asyncio
+import telegram
+
+# async def main():
+#     onsite_lsiuefgb_bot = telegram.Bot('5562358842:AAEhGUm4Bw6RJD4ItT6aop5nPR1XyJXyxX0')
+#     print('pause')
+#     async with onsite_lsiuefgb_bot:
+#         print(await onsite_lsiuefgb_bot.get_me())
+
+CHAT_ID_PRIVATE = 571775064
+CHAT_ID_BACKUP_GROUP = -651876374
+
+async def main():
+    with open('/home/syncoid/backup_log.txt', 'r') as log_file:
+        backup_msg = 'BACKUP EXECUTED - LOG:\n\n'
+        for line in log_file.readlines():
+            backup_msg += line
+
+        print(backup_msg)
+
+    onsite_bot = telegram.Bot('5562358842:AAEhGUm4Bw6RJD4ItT6aop5nPR1XyJXyxX0')
+#    offsite_bot = telegram.Bot('5484868873:AAFSw0_ceTr51OT7k2OHCzgrpc7qvc6h9bs')
+
+#    print('ONSITE:\n')
+#    for update_object in await read_updates(onsite_bot):
+#        print(update_object)
+#        print()
+
+#    print('OFFSITE:\n')
+#    for update_object in await read_updates(offsite_bot):
+#        print(update_object)
+#        print()
+
+#    await send_message(onsite_bot, text='Private Chat with OnsiteBot', chat_id=CHAT_ID_PRIVATE)
+#    await send_message(offsite_bot, text='Private Chat with OffsiteBot', chat_id=CHAT_ID_PRIVATE)
+    await send_message(onsite_bot, text=backup_msg, chat_id=CHAT_ID_BACKUP_GROUP)
+#    await send_message(offsite_bot, text='Group Chat from OffsiteBot', chat_id=CHAT_ID_BACKUP_GROUP)
+
+async def read_updates(bot):
+    async with bot:
+        return (await bot.get_updates())
+
+async def send_message(bot, text, chat_id):
+    async with bot:
+        await bot.send_message(text=text, chat_id=chat_id)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
