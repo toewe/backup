8a531dd0b7e925cee15d35ed3b1a7659d17108be
Simon Baumann <simonbmnn@gmail.com>
2023-10-18 22:28:54 +0200

    really first deployment attempt

--- stageing/telegram_utils.py
+++ stageing/telegram_utils.py
@@ -12,21 +12,19 @@ BOT_TOKEN: dict = {'onsite': '5562358842:AAEhGUm4Bw6RJD4ItT6aop5nPR1XyJXyxX0',
 def init_bot(token: str) -> telegram.Bot:
     return telegram.Bot(token)
 
-async def read_updates(bot):
+async def read_updates(bot: telegram.Bot):
     async with bot:
         return (await bot.get_updates())
 
-async def send_message(bot, text, chat_id):
+async def send_message(bot: telegram.Bot, text: str, chat_id: int):
     async with bot:
         await bot.send_message(text=text, chat_id=chat_id)
 
 def split_message_at_char_limit(msg: str, char_limit: int = MESSAGE_CHAR_LIMIT) -> list[str]:
 
-    msg_length: int = len(msg)
-    
     return_list: list[str] = []
 
-    for i in range(msg_length//char_limit + 1):
+    for i in range(len(msg)//char_limit + 1):
         return_list.append(msg[i*char_limit:(i+1)*char_limit])
 
     return return_list
